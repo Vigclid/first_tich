de3d2b5271238bd14309dedcef5361d6ef924a91
Đoàn Xuân Sơn <limbo04112004@gmail.com>
2025-01-13 16:24:17 +0700

    ok

--- src/first/main.java
+++ src/first/main.java
@@ -19,18 +19,7 @@ public class main {
         company.addTesla(new tesla(7,5,"pink",3080));
 
 
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(7);
-        list.add(9);
-        list.add(3);
-        list.add(4);
-
-
-        Collections.sort(company.getTeslas());
-
-        for (tesla tesla : company.getTeslas()) {
-            System.out.println(tesla);
-        }
+        System.out.println("Hello".compareTo("Hi"));
 
     }
 }
